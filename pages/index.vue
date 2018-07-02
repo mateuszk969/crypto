2920b83770a41472e06f03288c81f717fc37018a
mateuszk969 <mateuszk969@gmail.com>
2018-07-02 20:12:44 +0200

    t

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <h1 class="title">
-        crypto
-      </h1>
-      <h2 class="subtitle">
-        main page
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <ul class="links">
+        <li :key="post.date" v-for="post in posts">{{post.date}}</li>
+      </ul>
     </div>
   </section>
 </template>
