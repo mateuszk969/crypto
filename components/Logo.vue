c94924dd8f9ef13589b8fd86b309f297468892ab
mateuszk969 <mateuszk969@gmail.com>
2018-07-02 11:55:31 +0200

    mess

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,7 +3,6 @@
     <div class="Triangle Triangle--two"></div>
     <div class="Triangle Triangle--one"></div>
     <div class="Triangle Triangle--three"></div>
-    <div class="Triangle Triangle--four"></div>
   </div>
 </template>
 
