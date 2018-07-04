79790e7237d61ad0e9e0cd78b632d10688dbbd62
mateuszk969 <mateuszk969@gmail.com>
2018-07-04 11:34:31 +0200

    fix

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -7,11 +7,6 @@
 <script>
 export default {
   async asyncData({ params }) {
-    // const postPromise = process.BROWSER_BUILD
-    //   ? import('~/content/blog/posts/' + params.slug + '.json')
-    //   : Promise.resolve(
-    //       require('~/content/blog/posts/' + params.slug + '.json')
-    //     );
     let post = await import('~/content/blog/posts/' + params.slug + '.json');
     return post;
   }
