d9f3acb05db4d7ba1b04bc19353adda46425bee7
mateuszk969 <mateuszk969@gmail.com>
2018-07-17 14:32:36 +0200

    update

--- components/slider.vue
+++ components/slider.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="slider">
     <ul class="slides">
-        <div class="mainPost" :style="{backgroundImage: `url(${playslides.thumbnail ? playslides.thumbnail : slides[0].thumbnail})`}">
+        <div id="test" class="mainPost" :style="{backgroundImage: `url(${playslides.thumbnail ? playslides.thumbnail : slides[0].thumbnail})`}">
 					 <div class="postCont">
 						            <p class="date">{{playslides.date.substring(0,10)}} | {{playslides.comment}} comments</p>
                         <nuxt-link :to="playslides._path">
@@ -24,101 +24,140 @@
 
 
 <script>
-  export default {
-    data() {
-      return {
-			slides: JSON.parse(JSON.stringify(this.$store.getters.loadedPosts)).splice(1,2),
-    current: 0,
-    playslides: JSON.parse(JSON.stringify(this.$store.getters.loadedPosts[0])),
-  }
-    },
-  computed: {
+
+export default {
+  data() {
+    return {
+      slides: JSON.parse(
+        JSON.stringify(this.$store.getters.loadedPosts)
+      ).splice(1, 2),
+      current: 0,
+      playslides: JSON.parse(JSON.stringify(this.$store.getters.loadedPosts[0]))
+    };
   },
-    methods: {
-    selectSlide() { 
+  computed: {},
+  methods: {
+    selectSlide() {
       this.slides.push(this.playslides);
       this.playslides = this.slides[this.current];
-      this.slides.splice(this.current,1);
-			this.current++
-			if(this.current>this.slides.length){
-				this.current=0
-			}
-    },
+      this.slides.splice(this.current, 1);
+      this.current++;
+      if (this.current > this.slides.length) {
+        this.current = 0;
+      }
+    }
   },
   mounted() {
-		setInterval(this.selectSlide,3000);
+		setInterval(this.selectSlide, 10000);
+  setTimeout(function() {
+    document.getElementById('test').className += ' slideInDown';
+  }, 10000);
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 .slider {
-	 position: relative;
-	 z-index: 1;
-	 overflow: hidden;
-	 height: 60vh;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  height: 60vh;
 }
- .slider ul {
-	 list-style: none;
+.slider ul {
+  list-style: none;
 }
- .slider .slides {
-	 position: absolute;
-	 width: 100%;
-	 height: 100%;
-	 margin: 0;
-	 padding: 0;
-	 transition: top 800ms;
+.slider .slides {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  transition: top 800ms;
 }
- .slider .slides li {
-	 height: 100%;
+.slider .slides li {
+  height: 100%;
 }
- .slider .slides .mainPost {
-     width:100%;
-		 height:40vh;
-	 background-size: cover;
-	 background-position: 50% 50%;
-	 display:flex;
-	 justify-content: center;
-	 align-items: center;
+.slider .slides .mainPost {
+  width: 100%;
+  height: 40vh;
+  background-size: cover;
+  background-position: 50% 50%;
+  display: flex;
+  justify-content: center;
 }
- .slider .indicators {
-	 padding:0;
-	 display:flex;
-	 flex-direction: row;
-	 position: absolute;
-	 	bottom:0;
-		 width:100%;
+.slider .indicators {
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 }
-.indicators li{
-	 flex-basis: 50%;
+.indicators li {
+  flex-basis: 50%;
 }
-.item{
-	background-size: cover;
-	background-position: 50% 50%;
-	width:100%;
-	 height:20vh;
-	 display:flex;
-	   justify-content: center;
+.item {
+  background-size: cover;
+  background-position: 50% 50%;
+  width: 100%;
+  height: 20vh;
+  display: flex;
+  justify-content: center;
   align-items: flex-end;
 }
-.item .title{
-	padding:20px;
-	font-size:25px;
-	color:white;
-	text-align: center;
+.item .title {
+  padding: 20px;
+  font-size: 25px;
+  color: white;
+  text-align: center;
+}
+.postCont {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.postCont .title {
+  font-size: 20px;
+  color: white;
+  text-align: center;
+}
+.postCont .date {
+  font-size: 16px;
+  color: #cecdd1;
+}
+.postCont a,
+.item a {
+  text-decoration: none;
+}
 
+@-webkit-keyframes slideInDown {
+  0% {
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+    visibility: visible;
+  }
+
+  15% {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
 }
-.postCont .title{
-	font-size:30px;
-	color:white;
-		text-align: center;
+
+@keyframes slideInDown {
+  0% {
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+    visibility: visible;
+  }
+
+  15% {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
 }
-.postCont .date{
-	font-size:16px;
-color:	#CECDD1
+
+.slideInDown {
+	-webkit-animation: slideInDown 10s infinite;
+  animation: slideInDown 10s infinite;
 }
-.postCont a, .item a{
-		text-decoration: none;
-}
- 
 </style>
